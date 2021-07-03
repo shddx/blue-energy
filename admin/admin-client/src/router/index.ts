90a4491d6ced8af3a5a70b4db4d8a316a52b4354
Maxim Rodionov <kyarxadron1@gmail.com>
2021-07-04 00:27:59 +0300

    add houses view

--- admin/admin-client/src/router/index.ts
+++ admin/admin-client/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Container from '../layout/Container.vue'
 import Contracts from '../views/Contracts.vue'
+import Houses from '../views/Houses.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,6 +14,11 @@ const routes: Array<RouteRecordRaw> = [
         path: 'contracts',
         name: 'Contracts',
         component: Contracts
+      },
+      {
+        path: 'houses',
+        name: 'Houses',
+        component: Houses
       }
     ]
   }
---
+++ admin/admin-client/src/views/Houses.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  Houses
+</div>
+</template>
+
+<script>
+export default {
+  name: "Houses"
+}
+</script>
+
+<style scoped>
+
+</style>
