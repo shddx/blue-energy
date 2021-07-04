04ead83c7453f347d2cad4e317c7f7281c063452
Maxim Rodionov <kyarxadron1@gmail.com>
2021-07-04 13:45:02 +0300

    global cross origin config

--- admin/admin-backend/src/main/java/ru/rodionov/energo/admin/config/WebMvcConfiguration.java
+++ admin/admin-backend/src/main/java/ru/rodionov/energo/admin/config/WebMvcConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,14 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         resolver.setFallbackPageable(PageRequest.of(0, 20));
         resolver.setOneIndexedParameters(true);
         resolvers.add(resolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/admin/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
