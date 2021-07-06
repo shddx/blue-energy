14b88e57ba2f9ce8fced58235b2b9863021c16b7
Maxim Rodionov <kyarxadron1@gmail.com>
2021-07-06 13:01:36 +0300

    migrate to pinia done

--- admin/admin-client/src/store/modules/contract-module.ts
+++ admin/admin-client/src/store/modules/contract-module.ts
@@ -1,49 +1,31 @@
-import { VuexModule, Module, Mutation, Action } from "vuex-class-modules";
-import { store } from "@/store";
+import { defineStore } from "pinia";
+import { Contract } from "@/store/interfaces";
 import http from "@/api";
 import { CONTRACTS, CONTRACTS_SORTED } from "@/api/routes";
-import { Contract } from "@/store/interfaces";
 
-@Module
-class ContractModule extends VuexModule {
-  totalPages = 3
-  activePage = 1
-  contracts: Contract[][] = []
-
-  // getters
-
-  // mutations
-  @Mutation
-  setContracts (contracts: Contract[]) {
-    this.contracts[this.activePage] = contracts;
+export const useContractStore = defineStore({
+  id: 'contract-store',
+  state: () => ({
+    totalPages: 0,
+    activePage: 0,
+    contracts: Array<Array<Contract>>()
+  }),
+  actions: {
+    fetchContracts (page: number) {
+      http.get(CONTRACTS(page, 10))
+        .then(({data}) => {
+          this.activePage = page
+          this.totalPages = data.pages
+          this.contracts[page] = data.content
+        })
+        .catch(err => console.log(err))
+    },
+    fetchContractsSorted ({sort, direction}: { sort: string; direction: string }) {
+      http.get(CONTRACTS_SORTED(this.activePage, 10, sort, direction))
+        .then(({ data }) => {
+          this.contracts[this.activePage] = data.content
+        })
+        .catch(err => console.log(err))
+    }
   }
-  @Mutation
-  setPages (pages: number) {
-    this.totalPages = pages;
-  }
-  @Mutation
-  setActivePage (page: number) {
-    this.activePage = page;
-  }
-  @Action
-  async fetchContractsSorted ({sort, direction}: { sort: string; direction: string }) {
-    http.get(CONTRACTS_SORTED(this.activePage, 10, sort, direction))
-      .then(({ data }) => {
-        this.setContracts(data.content)
-      })
-      .catch(err => console.log(err))
-  }
-
-  @Action
-  async fetchContracts (page: number) {
-    await http.get(CONTRACTS(page, 10))
-      .then(({data}) => {
-        this.setActivePage(page)
-        this.setPages(data.pages)
-        this.setContracts(data.content)
-      })
-      .catch(err => console.log(err))
-  }
-}
-
-export const contractModule = new ContractModule({ store, name: "contract" });
+})
