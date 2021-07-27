6bd32c57feb335e5526dbdb72bba3fc6ff6e9956
Max <rodionov.m.dev@gmail.com>
2021-07-27 22:54:40 +0300

    mvn deploy fixes

--- admin/admin-backend/src/main/java/ru/rodionov/energo/admin/config/RDBMSConfig.java
+++ admin/admin-backend/src/main/java/ru/rodionov/energo/admin/config/RDBMSConfig.java
@@ -2,7 +2,9 @@ package ru.rodionov.energo.admin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
+@Profile("RDBMS")
 @Configuration
 @Import(ru.rodionov.energo.repository.rdbms.config.DBConfig.class)
 public class RDBMSConfig {
