3f890e0ea40d9ee2ac3025fb0ff198193e0d724a
Max <rodionov.m.dev@gmail.com>
2021-08-06 16:38:11 +0300

    frontend search implemented

--- admin/admin-client/src/api/routes.ts
+++ admin/admin-client/src/api/routes.ts
@@ -1,9 +1,14 @@
+import {ContractSearchParams} from "@/store/interfaces";
+
 export const CONTRACTS_SORTED = (page?: number, size?: number, sort?: string, direction?: string) => {
-  return `contract?page=${page}&size=${size}&sort=${sort},${direction}`
+    return `contract?page=${page}&size=${size}&sort=${sort},${direction}`
 }
 export const CONTRACTS = (page?: number, size?: number) => {
-  return `contract?page=${page}&size=${size}`
+    return `contract?page=${page}&size=${size}`
 }
 export const CONTRACT = (id: string) => {
-  return `contract/${id}`
+    return `contract/${id}`
+}
+export const CONTRACT_FILTER = (searchParams: ContractSearchParams) => {
+    return `contract/filter?${searchParams.buildSearchQuery()}`;
 }
--- admin/admin-client/src/store/interfaces.ts
+++ admin/admin-client/src/store/interfaces.ts
@@ -1,25 +1,58 @@
 export class Contract {
-  id: string;
-  contractNumber: string;
-  type: string;
-  client: string;
-  price: number;
-  signDate: string;
-  endDate: string;
-  updated: string;
+    id: string;
+    contractNumber: string;
+    type: string;
+    client: string;
+    price: number;
+    signDate: string;
+    endDate: string;
+    updated: string;
 
-  constructor(id?: string, contractNumber?: string, type?: string, client?: string, price?: number, signDate?: string, endDate?: string, updated?: string) {
-    this.id = id || '';
-    this.contractNumber = contractNumber || '';
-    this.type = type || '';
-    this.client = client || '';
-    this.price = price || 0;
-    this.signDate = signDate || '';
-    this.endDate = endDate || '';
-    this.updated = updated || '';
-  }
+    constructor(id?: string, contractNumber?: string, type?: string, client?: string, price?: number, signDate?: string, endDate?: string, updated?: string) {
+        this.id = id || '';
+        this.contractNumber = contractNumber || '';
+        this.type = type || '';
+        this.client = client || '';
+        this.price = price || 0;
+        this.signDate = signDate || '';
+        this.endDate = endDate || '';
+        this.updated = updated || '';
+    }
 }
+
+export class ContractSearchParams {
+    contractNumber: string | undefined;
+    client: string | undefined;
+    fromSignDate: string | undefined;
+    toSignDate: string | undefined;
+    priceLow: number | undefined;
+    priceHigh: number | undefined;
+    type: string | undefined;
+
+    constructor(contractNumber?: string, client?: string, fromSignDate?: string, toSignDate?: string, priceLow?: number, priceHigh?: number, type?: string) {
+        this.contractNumber = contractNumber;
+        this.client = client;
+        this.fromSignDate = fromSignDate;
+        this.toSignDate = toSignDate;
+        this.priceLow = priceLow;
+        this.priceHigh = priceHigh;
+        this.type = type;
+    }
+
+    buildSearchQuery = () => {
+        let params = [
+            ...(this.contractNumber ? ['contractNumber=' + this.contractNumber] : []),
+            ...(this.client ? ['client=' + this.client] : []),
+            ...(this.fromSignDate ? ['fromSignDate=' + this.fromSignDate] : []),
+            ...(this.toSignDate ? ['toSignDate=' + this.toSignDate] : []),
+            ...(this.priceLow ? ['priceLow=' + this.priceLow] : []),
+            ...(this.priceHigh ? ['priceHigh=' + this.priceHigh] : [])
+        ];
+        return params.join('&');
+    }
+}
+
 export interface Breadcrumb {
-  path: string,
-  name: string
+    path: string,
+    name: string
 }
